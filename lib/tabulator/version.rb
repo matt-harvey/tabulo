1af0c68015cbbcf6f0ab94f8b07e5b1139e17e17
Matthew Harvey <software@matthewharvey.net>
2017-03-03 19:36:59 +1100

    Initial commit

---
+++ lib/tabulator/version.rb
@@ -0,0 +1,3 @@
+module Tabulator
+  VERSION = "0.1.0"
+end
---
+++ spec/tabulator_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Tabulator do
+  it "has a version number" do
+    expect(Tabulator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
