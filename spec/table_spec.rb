1d78926416fe0a24ca3544aaef63cc57ac9c60a5
Matthew Harvey <software@matthewharvey.net>
2017-03-18 19:54:19 +1100

    Spec basic table formatting functionality.

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -2,8 +2,30 @@ require "spec_helper"
 
 describe Tabulator::Table do
 
+  let(:table) do
+    Tabulator::Table.new(1..5) do |t|
+      t.add_column("N", &:itself)
+      t.add_column("Doubled") { |n| n * 2 }
+    end
+  end
+
   pending "#initialize"
   pending "#add_column"
+
+  describe "#to_s" do
+    it "returns a string displaying formatted table" do
+      expect(table.to_s).to eq \
+        %q(+----------+----------+
+           |     N    |  Doubled |
+           +----------+----------+
+           |        1 |        2 |
+           |        2 |        4 |
+           |        3 |        6 |
+           |        4 |        8 |
+           |        5 |       10 |).gsub(/^ +/, "")
+    end
+  end
+
   pending "#each"
   pending "#header_row"
   pending "#horizontal_rule"
