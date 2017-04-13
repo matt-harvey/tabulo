5e43f9c60bbc563e17b611ae245322595749d845
Matthew Harvey <software@matthewharvey.net>
2017-04-13 19:48:01 +1000

    More specs

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -3,26 +3,72 @@ require "spec_helper"
 describe Tabulo::Table do
 
   let(:table) do
-    Tabulo::Table.new(1..5) do |t|
+    Tabulo::Table.new(1..5, header_frequency: header_frequency) do |t|
       t.add_column("N", &:itself)
       t.add_column("Doubled") { |n| n * 2 }
     end
+  end
+
+  let(:header_frequency) { [:start, nil, 5, 20].sample }
+
+  specify "is an Enumerable" do
+    expect(table).to be_a(Enumerable)
+    expect(table).to respond_to(:each)
+    expect(table).to respond_to(:map)
+    expect(table).to respond_to(:to_a)
   end
 
   pending "#initialize"
   pending "#add_column"
 
   describe "#to_s" do
-    it "returns a string displaying formatted table" do
-      expect(table.to_s).to eq \
-        %q(+----------+----------+
-           |     N    |  Doubled |
-           +----------+----------+
-           |        1 |        2 |
-           |        2 |        4 |
-           |        3 |        6 |
-           |        4 |        8 |
-           |        5 |       10 |).gsub(/^ +/, "")
+    context "when table was initialized with `header_frequency: :start`" do
+      let(:header_frequency) { :start }
+
+      it "returns a string displaying the formatted table with a header" do
+        expect(table.to_s).to eq \
+          %q(+----------+----------+
+             |     N    |  Doubled |
+             +----------+----------+
+             |        1 |        2 |
+             |        2 |        4 |
+             |        3 |        6 |
+             |        4 |        8 |
+             |        5 |       10 |).gsub(/^ +/, "")
+      end
+    end
+
+    context "when table was initialized with `header_frequency: nil`" do
+      let(:header_frequency) { nil }
+
+      it "returns a string displaying the formatted table without a header" do
+        expect(table.to_s).to eq \
+          %q(|        1 |        2 |
+             |        2 |        4 |
+             |        3 |        6 |
+             |        4 |        8 |
+             |        5 |       10 |).gsub(/^ +/, "")
+      end
+    end
+
+    context "when table was initialized with `header_frequency: <N>`" do
+      let(:header_frequency) { 3 }
+
+      it "returns a string displaying the formatted table with header at start and then "\
+        "before every Nth row thereafter" do
+        expect(table.to_s).to eq \
+          %q(+----------+----------+
+             |     N    |  Doubled |
+             +----------+----------+
+             |        1 |        2 |
+             |        2 |        4 |
+             |        3 |        6 |
+             +----------+----------+
+             |     N    |  Doubled |
+             +----------+----------+
+             |        4 |        8 |
+             |        5 |       10 |).gsub(/^ +/, "")
+      end
     end
   end
 
@@ -54,6 +100,21 @@ describe Tabulo::Table do
     end
   end
 
-  pending "#body_row"
-  pending "#formatted_body_row"
+  describe "#formatted_body_row" do
+    context "when passed `with_header: true`" do
+      it "returns a string representing a row in the body of the table, with a header" do
+        expect(table.formatted_body_row(3, with_header: true)).to eq \
+          %q(+----------+----------+
+             |     N    |  Doubled |
+             +----------+----------+
+             |        3 |        6 |).gsub(/^ +/, "")
+      end
+    end
+
+    context "when passed `with_header: false" do
+      it "returns a string representing a row in the body of the table, without a header" do
+        expect(table.formatted_body_row(3, with_header: false)).to eq("|        3 |        6 |")
+      end
+    end
+  end
 end
