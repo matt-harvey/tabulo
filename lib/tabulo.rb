9a8bd440c8e8895b928b92f17f4db77065296fa6
Matthew Harvey <software@matthewharvey.net>
2017-04-22 08:37:11 +1000

    TODO re. documentation

--- lib/tabulo.rb
+++ lib/tabulo.rb
@@ -3,6 +3,7 @@ require "tabulo/table"
 require "tabulo/row"
 require "tabulo/column"
 
+# TODO Document the methods.
 # TODO Tidy the code.
 # TODO Handle newline in output.
 # TODO Allow the option of a horizontal rule between rows.
