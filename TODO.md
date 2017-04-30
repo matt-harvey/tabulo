aab5a4e07405ac33749e3a1c8916502ab35d5716
Matthew Harvey <software@matthewharvey.net>
2017-04-30 13:40:26 +1000

    Write specs for Column.
    This takes care of all the pending specs.

--- TODO.md
+++ TODO.md
@@ -3,5 +3,4 @@
 * Consider incorporating a linter / static analysis tool into the build.
 * Handle multiline cell content (i.e. when the calculated cell value
   itself contains a newline).
-* Pending specs.
 * Fix broken appearance of http://www.rubydoc.info/gems/tabulo/<version> .
--- spec/column_spec.rb
+++ spec/column_spec.rb
@@ -1,11 +1,47 @@
 require "spec_helper"
 
 describe Tabulo::Column do
+  let(:column) do
+    Tabulo::Column.new(
+      label: :times_ten,
+      header: "X10",
+      width: 10,
+      align_header: :left,
+      align_body: :left,
+      formatter: -> (n) { "%.2f" % n },
+      extractor: -> (n) { n * 10 }
+    )
+  end
 
-  pending "#initialize"
-  pending "#label"
-  pending "#width"
-  pending "#header_cell"
-  pending "#body_cell"
-  pending "#body_cell_value"
+  describe "#initialize" do
+    it "create a Column" do
+      expect(column).to be_a(Tabulo::Column)
+    end
+  end
+
+  describe "#header_cell" do
+    it "returns a string being content for the column's header cell, with internal padding" do
+      expect(column.header_cell).to eq("X10       ")
+    end
+  end
+
+  describe "#body_cell" do
+    it "returns a string being the formatted content for this column for the passed source item, "\
+      "with internal padding" do
+      expect(column.body_cell(3)).to eq("30.00     ")
+    end
+  end
+
+  describe "#formatted_cell_content" do
+    it "returns a string being to formatted content for this column for the passed source item, "\
+      "without internal padding" do
+      expect(column.formatted_cell_content(3)).to eq("30.00")
+    end
+  end
+
+  describe "#body_cell_value" do
+    it "returns the underlying value in this column for the passed source item" do
+      expect(column.body_cell_value(3)).to eq(30)
+    end
+  end
 end
